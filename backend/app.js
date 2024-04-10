7b04a76676048b2caf8c38ed5f5c06be157c343a
Laryssa Medeiros <laryssamc.ted@gmail.com>
2024-04-10 00:03:36 -0300

    criando rotas

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,17 @@ app.use(cors())
 
 app.use(express.json())
 
+// DB Connection
+const conn = require("./db/conn");
+
+conn();
+
+//Routes
+const routes = require("./routes/router");
+
+app.use("/api", routes);
+
 app.listen(3000, function(){
     console.log("Servidor Online!")
 })
+
