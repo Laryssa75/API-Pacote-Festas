d086387bb78bc4de1e2e81ade9601055ca63802f
Laryssa Medeiros <67442535+Laryssa75@users.noreply.github.com>
2024-07-01 11:34:56 -0300

    finalizando api, só falta colocar o id corretos, gerados pelo banco.

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -1,5 +1,5 @@
 const PartyModel = require("../models/Party");
-const { getAll } = require("./serviceController");
+const { getAll, update } = require("./serviceController");
 
 const checkPartyBudget = (budget, services) => {
 
@@ -57,6 +57,7 @@ const partyController = {
 
             if(!party){
                 res.status(404).json({msg: "Festa não encontrada."})
+                return;
             }
 
             res.json(party);
@@ -64,6 +65,50 @@ const partyController = {
             console.log(error)
         }
     },
+    delete: async(req, res) => {
+        const id = req.params.id
+        const party = await PartyModel.findById(id);
+
+        if(!party) {
+            res.status(404).json({msg: "Festa não encontrada."})
+            return;
+        }
+
+        const deletedParty =  await PartyModel.findByIdAndDelete(id)
+
+        res.status(200).json({deletedParty, msg: "Festa excluída com sucesso!"});
+    },
+    update: async(req, res) => {
+        try {
+            
+            const id = req.params.id
+
+            const party = {
+                title: req.body.title,
+                author: req.body.author,
+                description: req.body.description,
+                budget: req.body.budget,
+                image: req.body.image,
+                services: req.body.services,
+            };
+
+            if(party.services && checkPartyBudget(party.budget, party.services)) {
+                res.status(406).json({msg: "O seu orçamento é insuficiente."});
+                return;
+            }
+
+            const updateParty = await PartyModel.findByIdAndUpdate(id, party);
+
+            if(!updateParty){
+                res.status(404).json({msg: "Festa não encontrada."});
+                return;
+            }
+
+            res.status(200).json({party, msg:"Festa atualizada com sucesso;"});
+        } catch (error) {
+            console.log(error)
+        }
+    }
 };
 
 module.exports = partyController;
